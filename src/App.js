eeef5e6762e1fcefcaede089417ebf9ee4f140af
AnhKhoa1408 <akhoa981@gmail.com>
2022-02-09 21:55:37 +0700

    Add login page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { PersistGate } from "redux-persist/lib/integration/react";
 import { persistor, store } from "./config/configureStore";
 import ErrorBoundary from "./components/ErrorHandle/ErrorHandle";
 import { toast, ToastContainer } from "react-toastify";
-import Layout from "./components/Layout";
+import { BrowserRouter } from "react-router-dom";
+import { RenderRoutes } from "./routes/RenderRoutes";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <ErrorBoundary>
-          <Layout />
-        </ErrorBoundary>
+        <BrowserRouter>
+          <ErrorBoundary>
+            <RenderRoutes />
+          </ErrorBoundary>
+        </BrowserRouter>
         <ToastContainer
           enableMultiContainer
           containerId="A"
