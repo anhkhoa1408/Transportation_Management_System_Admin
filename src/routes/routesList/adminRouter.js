d69d0ad8452d06947fbbc919facc77c07ce15715
AnhKhoa1408 <akhoa981@gmail.com>
2022-02-11 18:38:59 +0700

    Update router

--- src/routes/routesList/adminRouter.js
+++ src/routes/routesList/adminRouter.js
@@ -11,6 +11,58 @@ const CustomerInfo = lazy(() =>
   import("./../../views/DashboardPage/CustomerPage/CustomerInfo"),
 );
 
+const StaffList = lazy(() =>
+  import("./../../views/DashboardPage/StaffPage/StaffList"),
+);
+
+const StaffInfo = lazy(() =>
+  import("./../../views/DashboardPage/StaffPage/StaffInfo"),
+);
+
+const OrderList = lazy(() =>
+  import("./../../views/DashboardPage/OrderPage/OrderList"),
+);
+
+const OrderDetail = lazy(() =>
+  import("./../../views/DashboardPage/OrderPage/OrderDetail"),
+);
+
+const PackageList = lazy(() =>
+  import("./../../views/DashboardPage/PackagePage/PackageList"),
+);
+
+const PackageDetail = lazy(() =>
+  import("./../../views/DashboardPage/PackagePage/PackageDetail"),
+);
+
+const VehicleList = lazy(() =>
+  import("./../../views/DashboardPage/VehiclePage/VehicleList"),
+);
+
+const VehicleDetail = lazy(() =>
+  import("./../../views/DashboardPage/VehiclePage/VehicleDetail"),
+);
+
+const StorageList = lazy(() =>
+  import("./../../views/DashboardPage/StoragePage/StorageList"),
+);
+
+const StorageDetail = lazy(() =>
+  import("./../../views/DashboardPage/StoragePage/StorageDetail"),
+);
+
+const ReportList = lazy(() =>
+  import("./../../views/DashboardPage/ReportPage/ReportList"),
+);
+
+const ReportDetail = lazy(() =>
+  import("./../../views/DashboardPage/ReportPage/ReportDetail"),
+);
+
+const FeedbackList = lazy(() =>
+  import("./../../views/DashboardPage/FeedbackPage/FeedbackList"),
+);
+
 const adminRouter = [
   {
     link: "/dashboard",
@@ -23,6 +75,58 @@ const adminRouter = [
   {
     link: "/customer",
     component: CustomerList,
+  },
+  {
+    link: "/staff",
+    component: StaffList,
+  },
+  {
+    link: "/staff/info/:id",
+    component: StaffInfo,
+  },
+  {
+    link: "/order",
+    component: OrderList,
+  },
+  {
+    link: "/order/detail/:id",
+    component: OrderDetail,
+  },
+  {
+    link: "/package",
+    component: PackageList,
+  },
+  {
+    link: "/package/detail/:id",
+    component: PackageDetail,
+  },
+  {
+    link: "/vehicle",
+    component: VehicleList,
+  },
+  {
+    link: "/vehicle/detail/:id",
+    component: VehicleDetail,
+  },
+  {
+    link: "/storage",
+    component: StorageList,
+  },
+  {
+    link: "/storage/info/:id",
+    component: StorageDetail,
+  },
+  {
+    link: "/report",
+    component: ReportList,
+  },
+  {
+    link: "/report/detail/:id",
+    component: ReportDetail,
+  },
+  {
+    link: "/feedback",
+    component: FeedbackList,
   },
 ];
 
