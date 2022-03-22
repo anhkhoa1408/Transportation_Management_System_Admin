c1db4cb7a054e84718685cd7143c83d0e755ef43
AnhKhoa1408 <akhoa981@gmail.com>
2022-03-22 22:50:24 +0700

    add date picker

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import DateAdapter from '@mui/lab/AdapterDayjs';
-
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DatePicker from "@mui/lab/DatePicker";
 import TextField from "@mui/material/TextField";
 
-function TimePicker({ value, setValue }) {
+function TimePicker({ value, setValue, ...props }) {
   return (
     <LocalizationProvider dateAdapter={DateAdapter}>
       <DatePicker
-        label="Basic example"
         value={value}
         onChange={(newValue) => {
           setValue(newValue);
         }}
-        renderInput={(params) => <TextField {...params} />}
+        renderInput={(params) => <TextField className="w-100" {...params} />}
+        {...props}
+        
       />
     </LocalizationProvider>
   );
