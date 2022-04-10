7d4fd355525f5c07488614d31de6fc290bfd4457
AnhKhoa1408 <akhoa981@gmail.com>
2022-04-10 20:28:18 +0700

    update address util

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,9 +1,11 @@
 function joinAddress(addressObj) {
-  return `${addressObj.street}, ${addressObj.ward}, ${addressObj.province}, ${addressObj.city}`;
+  return `${addressObj?.street || ""}, ${addressObj?.ward || ""}, ${
+    addressObj?.province || ""
+  }, ${addressObj?.city || ""}`;
 }
 
 function simplifyString(string, count) {
-  return string.length < count ? string : string.slice(0, count).concat('...');
+  return string.length < count ? string : string.slice(0, count).concat("...");
 }
 
 export { joinAddress, simplifyString };
